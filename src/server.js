36c10adc3806131f3181ad60c677a3710579b656
Juan Carlos <ju.quesada@udla.edu.co>
2025-06-07 19:37:47 -0500

    puerto

--- src/server.js
+++ src/server.js
@@ -59,4 +59,5 @@ app.put("/recolecciones/:id", (req, res) => {
   });
 });
 
-app.listen(3001, () => console.log("Servidor corriendo en puerto 3001"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
